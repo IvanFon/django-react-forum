7984173d108e851a733cce039cd14449e1eea53c
Ivan Fonseca <ivanfon@riseup.net>
2019-08-26 23:00:59 -0400

    Add React router

---
+++ frontend/src/components/Boards.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Boards extends Component {
+  render() {
+    return (
+      <ul>
+        <li>Foo</li>
+        <li>Bar</li>
+      </ul>
+    );
+  }
+}
+
+export default Boards;
