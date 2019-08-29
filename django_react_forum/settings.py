2a24631851ef171b074435e901f149bbc529bb5d
Ivan Fonseca <ivanfon@riseup.net>
2019-08-28 20:57:30 -0400

    Switch to sliding JWT tokens

--- django_react_forum/settings.py
+++ django_react_forum/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+from datetime import timedelta
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -143,4 +144,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ]
+}
+
+# Simple JWT settings
+SIMPLE_JWT = {
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.SlidingToken',),
 }
--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+    TokenObtainSlidingView,
+    TokenRefreshSlidingView,
 )
 
 from .views import RegisterView
 
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainSlidingView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshSlidingView.as_view(),
+         name='token_refresh'),
     path('register/', RegisterView.as_view()),
 ]
