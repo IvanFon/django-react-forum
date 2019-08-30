d19081ad9fc41414a0267e39f8d6cdcdcbf1df29
Ivan Fonseca <ivanfon@riseup.net>
2019-08-30 17:36:09 -0400

    Add user tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,63 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from .models import User
+
+
+client = APIClient()
+
+
+class RegisterUserTest(TestCase):
+    """Test POST register user."""
+
+    def test_register_user(self):
+        # Register
+        res = client.post(reverse('register'),
+                          {
+                              'username': 'test',
+                              'password': 'test',
+                          },
+                          format='json')
+        # Verify response
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(User.objects.get(username='test')
+                                    .check_password('test'))
+
+
+class ObtainRefreshTokenTest(TestCase):
+    """Test POST obtain token."""
+
+    def setUp(self):
+        User.objects.create_user('test', None, 'test')
+
+    def obtain_token(self):
+        return client.post(reverse('token_obtain_pair'),
+                           {
+                               'username': 'test',
+                               'password': 'test',
+                           },
+                           format='json')
+
+    def test_obtain_token(self):
+        # Obtain tokens
+        res = self.obtain_token()
+        # Verify response
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertTrue('access' in res.data)
+        self.assertTrue('refresh' in res.data)
+
+    def test_refresh_token(self):
+        # Obtain tokens
+        data = self.obtain_token()
+        # Refresh tokens
+        res = client.post(reverse('token_refresh'),
+                          {
+                              'refresh': data.data['refresh'],
+                          },
+                          format='json')
+        # Verify response
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertTrue('access' in res.data)
+        self.assertTrue('refresh' in res.data)
--- users/urls.py
+++ users/urls.py
@@ -10,5 +10,5 @@ from .views import RegisterView
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view()),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
